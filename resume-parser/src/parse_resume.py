10160e564286b946f669948b5b8c546e1d09153e
Kristin Chen <kristin.chen@blend360.com>
2022-05-22 19:34:45 -0400

    Improve funs to get degree and get dates, add func to get schools

--- resume-parser/src/parse_resume.py
+++ resume-parser/src/parse_resume.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from dateutil import parser
 import pdfminer
 from pdfminer.high_level import extract_text
 import glob
@@ -15,6 +16,9 @@ nlp = spacy.load("en_core_web_sm")
 import os
 import pdfplumber
 import pandas as pd
+import datefinder
+from datetime import datetime
+from thefuzz import fuzz
 
 def read_file(path):
     txt = extract_text(path, codec='utf-8')
@@ -201,39 +205,259 @@ def extract_email(txt):
 # -----------------------------------
 def extract_phd_degree(txt):
     phd_set = set()
-    p = re.compile('(Ph\.D).*,|(Ph\.D).*\\n')
+    p = re.compile('(Ph\.D|PhD|PhD\.).*,|(Ph\.D|PhD|PhD\.).*\\n')
     for m in re.finditer(p, txt):
         candidate_txt = m.group().split(',')[0]
         phd_set.add(' '.join(candidate_txt.split()))
     return phd_set
 
-
+def extract_phd_degree_from_lines(txt):
+    lines_txt = [l.strip() for l in txt.split('\n') if len(l.strip()) > 0 and len(l.strip().split()) > 0]
+    phd_degrees_lst = []
+    for index, line in enumerate(lines_txt):
+        if re.match('(Ph\.D|PhD|PhD\.)' ,line):
+            phd_degrees_lst.append(line.strip())
+    return [' '.join(m.split()) for m in phd_degrees_lst]
 
 # ------------------------------------
+neglect_words = [
+    'MSSQL',
+    'MS LAB',
+    'MSMQ',
+    'MS-DB',
+    'MS-Excel',
+    'MS-Access',
+    'MATLAB',
+    'MARY',
+    'MACHINE',
+    'MAPE',
+    'MSE',
+    'MARKOV',
+    'MAXIMIZATION',
+    'MATPLOTLIB',
+    'MATPLOT',
+    'MATPLOTLIB',
+    'MATRICES',
+    'MATRIX',
+    'MASSACHUSETTS',
+    'Mssql',
+    'Ms Lab',
+    'Msmq',
+    'Ms-db',
+    'Ms-excel',
+    'Ms-access',
+    'Matlab',
+    'Mary',
+    'Machine',
+    'Mape',
+    'Mse',
+    'Markov',
+    'Maximization',
+    'Matplotlib',
+    'Matplot',
+    'Matplotlib',
+    'Matrices',
+    'Matrix',
+    'Massachusetts'
+    'mssql',
+    'ms lab',
+    'msmq',
+    'ms-db',
+    'ms-excel',
+    'ms-access',
+    'matlab',
+    'mary',
+    'machine',
+    'mape',
+    'mse',
+    'markov',
+    'maximization',
+    'matplotlib',
+    'matplot',
+    'matplotlib',
+    'matrices',
+    'matrix',
+    'massachusetts'
+]
+
 def extract_master_degree(txt):
+    ms_set = set()
     masters_set = set()
-    p = re.compile('(Master|MS|M\.S\.|MA|M\.A\.|MBA|M\.S\.E|M\.tech).*,|(Master|MS|M\.S\.|MA|M\.A\.|MBA|M\.S\.E|M\.tech).*\\n')  # greedy
+    p = re.compile('((?i:Master)|MS|M\.S\.|MA|M\.A\.|MBA|M\.S\.E|M\.tech).*,|((?i:Master)|MS|M\.S\.|MA|M\.A\.|MBA|M\.S\.E|M\.tech).*\\n')  # greedy
     for m in re.finditer(p, txt):
-        candidate_txt = m.group().split(',')[0] #Comment: to avoid getting 'MS Excel MS word'....
-        if 'Master' in candidate_txt:
-            return ' '.join(candidate_txt.split()) #immediate return master
-        else:
+        candidate_txt = m.group().split(',')[0]
+        for w in candidate_txt.split():
+            if w in neglect_words:
+                continue
+            else:
+                ms_set.add(' '.join(candidate_txt.split()))
+        if re.search('(?i:Master)', candidate_txt):
+            print(candidate_txt)
             masters_set.add(' '.join(candidate_txt.split()))
-    return masters_set
+    if len(masters_set) > 0:
+        return masters_set
+    else:
+        return ms_set
 
+def is_valid_date(txt):
+    return_lst = []
+    matches = datefinder.find_dates(txt)
+    for match in matches:
+        #print(match)
+        return_lst.append(match)
+    if len(return_lst):
+        return True
+    else:
+        return False
 
-
+def extract_master_degree_from_lines(txt):
+    year ="(\d{4})"
+    month = "((?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Sept|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?))"
+    lines_txt = [l.strip() for l in txt.split('\n') if len(l.strip()) > 0 and len(l.strip().split()) > 0]
+    master_degrees_lst = []
+    ms_lst = []
+    for index, line in enumerate(lines_txt):
+        if re.match('(?i:Master)' ,line):
+            master_degrees_lst.append(line.strip())
+        if re.match('MS|M\.S\.|MA|M\.A\.|MBA|M\.S\.E|M\.tech' ,line):
+            ms_lst.append(line.strip())
+    if len(master_degrees_lst) > 0:
+        # cleaned_master_degress_lst = []
+        # for d in master_degrees_lst:
+        #     cleaned_master_degress_lst.append(' '.join([m for m in d.split() if m != month or not m.isdigit()]))
+        return [' '.join(m.split()) for m in master_degrees_lst]
+    else:
+        return [' '.join(m.split()) for m in ms_lst]
 # ---------------------------------
+# doesn't match: \nBachelors of Management Studies (concentration in marketing)
 def extract_bachelor_degree(txt):
     bachelors_set = set()
-    p = re.compile('(Bachelor|BS|B\.S\.|BA|B\.A\.|B\.S\.E|B\.tech).*,|(Bachelor|BS|B\.S\.|BA|B\.A\.|B\.S\.E|B\.tech).*\\n')  # greedy
+    ba_set = set()
+    p = re.compile('((?i:Bachelor)|BS|B\.S\.|BA|B\.A\.|B\.S\.E|B\.tech).*,|((?i:Bachelor)|BS|B\.S\.|BA|B\.A\.|B\.S\.E|B\.tech).*\\n')  # greedy
     for m in re.finditer(p, txt):
-        candidate_txt = m.group().split(',')[0] #MS Excel MS word....
-        if 'Bachelor' in candidate_txt:
-            return ' '.join(candidate_txt.split()) #immediate return master
-        else:
+        candidate_txt = m.group().split(',')[0] #MS Excel MS word...
+        print(candidate_txt)
+        ba_set.add(' '.join(candidate_txt.split()))
+        if re.search('(?i:Bachelor)', candidate_txt):
             bachelors_set.add(' '.join(candidate_txt.split()))
-    return bachelors_set
+        if len(bachelors_set) > 0:
+            return bachelors_set
+        else:
+            return ba_set
+
+def extract_bachelor_degree_from_lines(txt):
+    lines_txt = [l.strip() for l in txt.split('\n') if len(l.strip()) > 0 and len(l.strip().split()) > 0]
+    bachelors_set = []
+    ba_set = []
+    for index, line in enumerate(lines_txt):
+        if re.match('(?i:Bachelor)' ,line):
+            bachelors_set.append(line.strip())
+        if re.match('BS|B\.S\.|BA|B\.A\.|B\.S\.E|B\.tech' ,line):
+            ba_set.append(line.strip())
+
+    if len(bachelors_set) > 0:
+        return [' '.join(m.split()) for m in bachelors_set]
+    else:
+        return [' '.join(m.split()) for m in ba_set]
+
+# -----------------------------------------------------
+def extract_phd_school(txt):
+    p = re.compile(
+        '(?i)(school|college|univers|academy|faculty|institute|faculdades|Schola|schule|lise|lyceum|lycee|polytechnic|kolej|ünivers|okul|University)(.|\n)*(Ph.D.|PhD|PhD.)')
+    for m in re.finditer(p, txt):
+        return m.group(0).split("\n")[0]
+
+def extract_ms_school(txt):
+    p = re.compile(
+        '(?i)(school|college|univers|academy|faculty|institute|faculdades|Schola|schule|lise|lyceum|lycee|polytechnic|kolej|ünivers|okul|University)(.|\n)*(M.S.|M.S|M.Sc.|MS|Masters|(?i:Master))')
+    for m in re.finditer(p, txt):
+        return m.group(0).split("\n")[0]
+
+
+## Using regex for finding university nearest to Bachelor's degree
+def extract_bach_school(txt):
+    p = re.compile(
+        '(?i)(school|college|academy|faculty|institute|faculdades|Schola|schule|lise|lyceum|lycee|polytechnic|kolej|ünivers|okul|University)(?:(?!(?i)(school|college|academy|faculty|institute|faculdades|Schola|schule|lise|lyceum|lycee|polytechnic|kolej|ünivers|okul|University)|(Bachelors|B.tech|Bachelor\'s|B.A.|BS|B.Sc.|B.S|B.S.))[\s\S])*(Bachelors|B.tech|Bachelor\'s|B.A.|BS|B.Sc.|B.S|B.S.)')
+    for m in re.finditer(p, txt):
+        return m.group(0).split("\n")[0]
+
+# date -------------------------------------------------------------------------
+
+
+
+
+
+def extract_master_date(txt1):
+    lines_txt = [l.strip() for l in txt1.split('\n') if len(l.strip()) > 0 and len(l.strip().split()) > 0]
+
+    for index, line in enumerate(lines_txt):
+            if re.match('(?i:Master)' ,line):
+                masters_date_lst = []
+                start_idx = index
+                for i, l in enumerate(lines_txt[start_idx:start_idx + 8]):
+                    print(i, l)
+                    if datefinder.find_dates(l):
+                        matches = datefinder.find_dates(l)
+                        for match in matches:
+                            print(match)
+                            masters_date_lst.append(match)
+                        if len(masters_date_lst) > 0:
+                            return [d.strftime("%m/%Y") for d in masters_date_lst]
+            elif re.match('MS|M\.S\.|MA|M\.A\.|MBA|M\.S\.E|M\.tech' ,line):
+                return_set = []
+                start_idx = index
+                for _, l in enumerate(lines_txt[start_idx:start_idx + 8]):
+                    if datefinder.find_dates(l):
+                        matches = datefinder.find_dates(l)
+                        for match in matches:
+                            print(match)
+                            return_set.append(match)
+                        return [d.strftime("%m/%Y") for d in return_set]
+
+def extract_bachelor_date(txt1):
+    lines_txt = [l.strip() for l in txt1.split('\n') if len(l.strip()) > 0 and len(l.strip().split()) > 0]
+
+    for index, line in enumerate(lines_txt):
+        if re.match('(?i:Bachelor)', line):
+            bachelor_date_lst = []
+            start_idx = index
+            for i, l in enumerate(lines_txt[start_idx:start_idx + 8]):
+                print(i, l)
+                if datefinder.find_dates(l):
+                    matches = datefinder.find_dates(l)
+                    for match in matches:
+                        print(match)
+                        bachelor_date_lst.append(match)
+                    if len(bachelor_date_lst) > 0:
+                        return [d.strftime("%m/%Y") for d in bachelor_date_lst]
+        elif re.match('BS|B\.S\.|BA|B\.A\.|B\.S\.E|B\.tech' ,line):
+            return_set = []
+            start_idx = index
+            for _, l in enumerate(lines_txt[start_idx:start_idx + 8]):
+                if datefinder.find_dates(l):
+                    matches = datefinder.find_dates(l)
+                    for match in matches:
+                        print(match)
+                        return_set.append(match)
+                    return [d.strftime("%m/%Y") for d in return_set]
+
+def extract_phd_date(txt1):
+    lines_txt = [l.strip() for l in txt1.split('\n') if len(l.strip()) > 0 and len(l.strip().split()) > 0]
+    phd_date_lst = []
+    for index, line in enumerate(lines_txt):
+        if re.match('(Ph\.D|PhD|PhD\.)' ,line):
+            start_idx = index
+            for i, l in enumerate(lines_txt[start_idx:start_idx + 8]):
+                print(i, l)
+                if datefinder.find_dates(l):
+                    matches = datefinder.find_dates(l)
+                    for match in matches:
+                        print(match)
+                        phd_date_lst.append(match)
+                    if len(phd_date_lst) > 0:
+                        return [d.strftime("%m/%Y") for d in phd_date_lst]
+
+    return phd_date_lst
 
 
 
@@ -247,9 +471,19 @@ def get_info(txt):
     # education = extract_education(txt)
     phone_number = extract_phone_number(txt)
     email = extract_email(txt)
-    master = extract_master_degree(txt)
-    bachelor = extract_bachelor_degree(txt)
-    phd = extract_phd_degree(txt)
+    master_degree = extract_master_degree(txt)
+    bachelor_degree = extract_bachelor_degree(txt)
+    phd_degree = extract_phd_degree(txt)
+    master_degree2 = extract_master_degree_from_lines(txt)
+    bachelor_degree2 = extract_bachelor_degree_from_lines(txt)
+    phd_degree2 = extract_phd_degree_from_lines(txt)
+
+    master_school = extract_ms_school(txt)
+    bachelor_school = extract_bach_school(txt)
+    phd_school = extract_phd_school(txt)
+    master_graduation_date = extract_master_date(txt)
+    bachelor_graduation_date = extract_bachelor_date(txt)
+    phd_graduation_date = extract_phd_date(txt)
     return {
         'name' : name,
         'name_ner': name_ner,
@@ -257,9 +491,18 @@ def get_info(txt):
         'name_ref': name_ref,
         'phone_number' : phone_number,
         'email' : email,
-        'master_degree' : master,
-        'bachelor_degree': bachelor,
-        'phd_degree' : phd
+        'master_degree' : master_degree,
+        'bachelor_degree': bachelor_degree,
+        'phd_degree' : phd_degree,
+        'master_degree2': master_degree2,
+        'bachelor_degree2': bachelor_degree2,
+        'phd_degree2': phd_degree2,
+        'master_school': master_school,
+        'bachelor_school': bachelor_school,
+        'phd_school': phd_school,
+        'master_graduation_date': master_graduation_date,
+        'bachelor_graduation_date': bachelor_graduation_date,
+        'phd_graduation_date': phd_graduation_date
     }
 
 def parse_resume(path):
@@ -277,7 +520,17 @@ def parse_resume(path):
                    'email': None,
                    'master_degree': None,
                    'bachelor_degree': None,
-                   'phd_degree': None}
+                   'phd_degree': None,
+                   'master_degree2': None,
+                   'bachelor_degree2': None,
+                   'phd_degree2': None,
+                   'master_school': None,
+                   'bachelor_school': None,
+                   'phd_school': None,
+                   'master_graduation_date': None,
+                   'bachelor_graduation_date': None,
+                   'phd_graduation_date': None
+                       }
         return return_dict
 
     if not isinstance(raw_txt, str):
@@ -290,7 +543,16 @@ def parse_resume(path):
                           'email': None,
                           'master_degree': None,
                           'bachelor_degree': None,
-                          'phd_degree': None}
+                          'phd_degree': None,
+                          'master_degree2': None,
+                          'bachelor_degree2': None,
+                          'phd_degree2': None,
+                          'master_school': None,
+                          'bachelor_school': None,
+                          'phd_school': None,
+                          'master_graduation_date': None,
+                          'bachelor_graduation_date': None,
+                          'phd_graduation_date': None}
         return return_dict
     elif len(raw_txt) < 8:
         return_dict = {   'index' : index,
@@ -302,7 +564,17 @@ def parse_resume(path):
                           'email': None,
                           'master_degree': None,
                           'bachelor_degree': None,
-                          'phd_degree': None}
+                          'phd_degree': None,
+                          'master_degree2': None,
+                          'bachelor_degree2': None,
+                          'phd_degree2': None,
+                          'master_school': None,
+                          'bachelor_school': None,
+                          'phd_school': None,
+                          'master_graduation_date': None,
+                          'bachelor_graduation_date': None,
+                          'phd_graduation_date': None}
+        return return_dict
     else:
         info_dict = get_info(raw_txt)
         return_dict = {**{'index': index}, **info_dict}
@@ -310,26 +582,29 @@ def parse_resume(path):
 
 # -------------- execution ------------------------------------------------------
 folder = glob.glob(r"./data/resume_samples/*")
-# file_path = folder[2]
-#
-# sample_txt = read_file(file_path).decode('utf-8')
-# sample_txt = read_file2(file_path)
+# file_path = folder[11]
+# sample_txt = read_file(file_path)
+# sample_txt2 = read_file2(file_path)
 # tokens_txt = preprocess_text(sample_txt)
-#
-#
 #
 # get_name(sentence_txt=sample_txt, tokens_txt = tokens_txt)
 # get_name_from_ner(sample_txt)
 # get_name_from_pos(tokens_txt)
 # get_name_from_ref(tokens_txt)
-#
 # extract_education(sample_txt[0:1000])
 # doc = nlp(sample_txt)
 # extract_phd_degree(sample_txt)
-# doc = nlp(sample_txt)
+# extract_phd_degree_from_lines(sample_txt)
+# extract_phd_school(sample_txt)
+# extract_phd_date(sample_txt)
 # extract_master_degree(sample_txt)
-# doc = nlp(sample_txt)
+# extract_master_degree_from_lines(sample_txt)
+# extract_ms_school(sample_txt)
+# extract_master_date(sample_txt)
 # extract_bachelor_degree(sample_txt)
+# extract_bachelor_degree_from_lines(sample_txt)
+# extract_bach_school(sample_txt)
+# extract_bachelor_date(sample_txt)
 
 df = pd.DataFrame()
 for f in folder:
@@ -337,4 +612,11 @@ for f in folder:
     df = df.append(return_dict, ignore_index = True)
 
 df.to_csv('matched_resumes.csv', index=False)
+
+# # --------- performance ------------------------------
+# gt_df = pd.read_csv('./Data/GroundTruth-Sheet2.csv', index_col=False)
+# gt_df.shape #182
+# gt_df.columns
+# gt_df.master_graduation_date
+# fuzz.token_sort_ratio("KRISTIN J CHEN", "KRISTIN JIATING CHEN")
 
